b6a669b0ea5135b9ede00069cff3aa8cafe43e91
fuyi <1406751952@qq.com>
2017-11-28 15:06:18 +0800

    add ribbon without eureka properties config

---
+++ microservice-spring-cloud-parent/microservice-consumer-movie-ribbon-without-eureka/src/main/java/com/fuyi/cloud/controller/MovieController.java
@@ -0,0 +1,42 @@
+package com.fuyi.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fuyi.cloud.entity.User;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id) {
+		//http://localhost:1111
+		//vip virtual IP   microservice-provider-user:1111
+		ServiceInstance si = this.loadBalancerClient.choose("microservice-provider-user");
+		System.out.println("aaaaaa  == " + si.getServiceId() + " : " + si.getHost() + " : " + si.getPort());
+
+		return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+	}
+	
+	@GetMapping("/text")
+	public String text() {
+		ServiceInstance si = this.loadBalancerClient.choose("microservice-provider-user");
+		System.out.println("aaaaaa  == " + si.getServiceId() + " : " + si.getHost() + " : " + si.getPort());
+		
+		ServiceInstance si2 = this.loadBalancerClient.choose("microservice-provider-user2");
+		System.out.println("bbbbbb  == " + si2.getServiceId() + " : " + si2.getHost() + " : " + si2.getPort());
+		
+		return "1";
+	}
+}
