96f5584edc345fda04deb7c037d9e0c2e9494f48
fuyi <1406751952@qq.com>
2017-12-01 17:30:55 +0800

    test zuul fallback

---
+++ microservice-spring-cloud-parent/microservice-gateway-zuul-fallback/src/main/java/com/fuyi/cloud/fallback/MyFallbackProvider.java
@@ -0,0 +1,60 @@
+package com.fuyi.cloud.fallback;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFallbackProvider implements ZuulFallbackProvider {
+
+	@Override
+	public String getRoute() {
+		return "microservice-provider-user-v1";
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse() {
+		return new ClientHttpResponse() {
+			
+			@Override
+			public HttpHeaders getHeaders() {
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				return headers;
+			}
+			
+			@Override
+			public InputStream getBody() throws IOException {
+				return new ByteArrayInputStream("fallback".getBytes());
+			}
+			
+			@Override
+			public String getStatusText() throws IOException {
+				return "OK";
+			}
+			
+			@Override
+			public HttpStatus getStatusCode() throws IOException {
+				return HttpStatus.OK;
+			}
+			
+			@Override
+			public int getRawStatusCode() throws IOException {
+				return 200;
+			}
+			
+			@Override
+			public void close() {
+				
+			}
+		};
+	}
+
+}
