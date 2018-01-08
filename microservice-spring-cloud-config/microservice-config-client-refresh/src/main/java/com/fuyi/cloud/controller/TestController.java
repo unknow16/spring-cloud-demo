b41d1670bd2c38453ac9793c661c84391f8a47f4
fuyi <1406751952@qq.com>
2018-01-08 15:27:48 +0800

    test spring cloud config

--- microservice-spring-cloud-config/microservice-config-client-refresh/src/main/java/com/fuyi/cloud/controller/TestController.java
+++ microservice-spring-cloud-config/microservice-config-client-refresh/src/main/java/com/fuyi/cloud/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.fuyi.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
 
-	@Value("${notify-host}")
+	//@Value("${notify-host}")
+	@Value("${profile}")
 	private String profile;
 	
 	@GetMapping("/profile")
